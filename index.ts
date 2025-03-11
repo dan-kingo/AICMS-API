bfd16fe74d7978c66098255b0bfc2a8caff81aa4
Dan Kingo <danieldejen23@gmail.com>
2025-03-11 15:28:23 +0300

    enable debuger for ease of simplicity

--- index.ts
+++ index.ts
@@ -1,19 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
+import cors from "cors";
+import debug from "debug";
+
 import accessLogStream from "./middlewares/loggerMiddleware";
 import corsOptions from "./middlewares/corsOptionsMiddleware";
-import cors from "cors";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
+const appDebug = debug("app:startup");
 
 app.use(express.json());
-app.use(morgan("combined", { stream: accessLogStream }));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("combined", { stream: accessLogStream }));
+}
 app.use(cors(corsOptions));
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Home page");
 });
 app.listen(PORT, () => {
-  console.log(`server started at port ${PORT}`);
+  appDebug(`server started at port ${PORT}`);
 });
