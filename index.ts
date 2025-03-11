26ac751b1a41ed1ba39ee2902d6eb8309aa9e4bd
Dan Kingo <danieldejen23@gmail.com>
2025-03-11 15:15:04 +0300

    add logger middleware

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
+import accessLogStream from "./middlewares/loggerMiddleware";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Home page");
