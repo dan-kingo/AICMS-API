0f961357f09b0a6860490e93c4d7fedf536beb7b
Dan Kingo <danieldejen23@gmail.com>
2025-03-11 20:46:50 +0300

    connect to database

--- index.ts
+++ index.ts
@@ -7,16 +7,25 @@ dotenv.config();
 
 import accessLogStream from "./middlewares/loggerMiddleware";
 import corsOptions from "./middlewares/corsOptionsMiddleware";
+import mongoose from "mongoose";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 const appDebug = debug("app:startup");
+const dbDebug = debug("app:db");
 
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("combined", { stream: accessLogStream }));
 }
 app.use(cors(corsOptions));
+
+// connect to database
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => dbDebug("connected to database!"))
+  .catch((err) => dbDebug(err));
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Home page");
