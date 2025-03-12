424a8a7ee2891e40606b19994f1d649b07fce207
Dan Kingo <danieldejen23@gmail.com>
2025-03-12 09:14:03 +0300

    apply validation middleware

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { register } from "../controllers/authController";
+import { validateData } from "../middlewares/validationMiddleware";
+import registerSchema from "../schemas/registerSchema";
 
 const authRouter = Router();
 
-authRouter.post("/auth/register", register);
+authRouter.post("/auth/register", validateData(registerSchema), register);
 
 export default authRouter;
