09a16d398742a784bd131e812aa2e379b140d68c
Dan Kingo <danieldejen23@gmail.com>
2025-03-12 10:34:40 +0300

    handle user update route

--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { getCurrentUser } from "../controllers/userController";
+import { getCurrentUser, updateUser } from "../controllers/userController";
 import authMiddleware from "../middlewares/authMiddleware";
+import { validateData } from "../middlewares/validationMiddleware";
+import registerSchema from "../schemas/registerSchema";
 
 const userRouter = Router();
 
 userRouter.get("/user/current-user", authMiddleware, getCurrentUser);
+userRouter.post(
+  "/user/update-user",
+  authMiddleware,
+  validateData(registerSchema),
+  updateUser
+);
 
 export default userRouter;
