35987512a5255b1b2bc49ed73a17b53444e822c9
Dan Kingo <danieldejen23@gmail.com>
2025-04-17 09:14:52 +0300

    fix __dirname issues in multer

--- middlewares/multerMiddleware.ts
+++ middlewares/multerMiddleware.ts
@@ -2,6 +2,9 @@ import multer, { StorageEngine } from "multer";
 import path from "path";
 import fs from "fs";
 import { Request } from "express";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const uploadDir = path.join(__dirname, "..", "uploads");
 if (!fs.existsSync(uploadDir)) {
